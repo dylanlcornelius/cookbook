99390d75ec28a65bff7dda2d5e501cfcfa5e6e28
Dylan <dylanlcornelius@gmail.com>
2018-09-09 15:17:10 -0400

    Creation of main form.

---
+++ MealPlanner/MealPlanner/MainForm.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace MealPlanner
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.pnlRecipes = new System.Windows.Forms.Panel();
+            this.btnRecipes = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.pnlRecipes.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
+            this.btnClose.FlatAppearance.BorderSize = 0;
+            this.btnClose.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
+            this.btnClose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.ForeColor = System.Drawing.Color.Gray;
+            this.btnClose.Location = new System.Drawing.Point(963, 12);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(25, 25);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.Text = "X";
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            this.btnClose.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnClose_MouseDown);
+            this.btnClose.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btnClose_MouseUp);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
+            this.lblTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblTitle.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.Gray;
+            this.lblTitle.Location = new System.Drawing.Point(0, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Padding = new System.Windows.Forms.Padding(10);
+            this.lblTitle.Size = new System.Drawing.Size(1000, 50);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "Meal Planner";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblTitle.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lblTitle_MouseDown);
+            this.lblTitle.MouseMove += new System.Windows.Forms.MouseEventHandler(this.lblTitle_MouseMove);
+            this.lblTitle.MouseUp += new System.Windows.Forms.MouseEventHandler(this.lblTitle_MouseUp);
+            // 
+            // pnlRecipes
+            // 
+            this.pnlRecipes.Controls.Add(this.dataGridView1);
+            this.pnlRecipes.Location = new System.Drawing.Point(12, 78);
+            this.pnlRecipes.Name = "pnlRecipes";
+            this.pnlRecipes.Size = new System.Drawing.Size(976, 510);
+            this.pnlRecipes.TabIndex = 2;
+            // 
+            // btnRecipes
+            // 
+            this.btnRecipes.FlatAppearance.BorderSize = 0;
+            this.btnRecipes.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black;
+            this.btnRecipes.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Black;
+            this.btnRecipes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRecipes.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRecipes.ForeColor = System.Drawing.Color.Gray;
+            this.btnRecipes.Location = new System.Drawing.Point(12, 50);
+            this.btnRecipes.Margin = new System.Windows.Forms.Padding(0);
+            this.btnRecipes.Name = "btnRecipes";
+            this.btnRecipes.Size = new System.Drawing.Size(75, 25);
+            this.btnRecipes.TabIndex = 3;
+            this.btnRecipes.Text = "Recipes";
+            this.btnRecipes.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(231, 159);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(398, 172);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1000, 600);
+            this.Controls.Add(this.btnRecipes);
+            this.Controls.Add(this.pnlRecipes);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.lblTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Meal Planner";
+            this.pnlRecipes.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Panel pnlRecipes;
+        private System.Windows.Forms.Button btnRecipes;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ MealPlanner/MealPlanner/MainForm.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MealPlanner
+{
+    public partial class MainForm : Form
+    {
+        private bool isMouseDown = false;
+        private Point firstPoint;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        #region TITLE BAR
+
+        private void lblTitle_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isMouseDown)
+            {
+                int x = Location.X - firstPoint.X + e.X;
+                int y = Location.Y - firstPoint.Y + e.Y;
+                Location = new Point(x, y);
+                Update();
+            }
+        }
+
+        private void lblTitle_MouseDown(object sender, MouseEventArgs e)
+        {
+            firstPoint = e.Location;
+            isMouseDown = true;
+        }
+
+        private void lblTitle_MouseUp(object sender, MouseEventArgs e)
+        {
+            isMouseDown = false;
+        }
+
+        private void btnClose_MouseDown(object sender, MouseEventArgs e)
+        {
+            btnClose.ForeColor = Color.Silver;
+        }
+
+        private void btnClose_MouseUp(object sender, MouseEventArgs e)
+        {
+            btnClose.ForeColor = Color.Gray;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        #endregion
+    }
+}
