aeb91d06af8ed165e1a578ea13e1ada0f8f47d72
Dylan Cornelius <dylan.cornelius@fotlinc.com>
2019-07-19 09:00:13 -0500

    Created profile page

--- src/app/profile/profile/profile.component.ts
+++ src/app/profile/profile/profile.component.ts
@@ -1,4 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Notification } from 'src/app/modals/notification-modal/notification.enum';
+import {
+  FormControl,
+  FormBuilder,
+  FormGroup,
+  Validators
+} from '@angular/forms';
+import { CookieService } from 'ngx-cookie-service';
+import { ErrorStateMatcher } from '@angular/material';
+import { UserService } from '../../user/user.service';
+import { User } from 'src/app/user/user.model';
+
+class ErrorMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null): boolean {
+    return (control && control.invalid && (control.dirty || control.touched));
+  }
+}
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +24,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  loading: Boolean = true;
+  notificationModalParams;
+
+  userForm: FormGroup;
+  uid: string;
+  id: string;
+
+  matcher = new ErrorMatcher();
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private cookieService: CookieService,
+    private userService: UserService,
+  ) { }
 
   ngOnInit() {
+    // TODO: make uid global
+    this.uid = this.cookieService.get('LoggedIn');
+    this.initializeUserForm();
   }
 
+  initializeUserForm() {
+    this.loading = true;
+
+    this.userService.getUser(this.uid).subscribe((user: User) => {
+      this.userForm = this.formBuilder.group({
+        uid: [this.uid],
+        firstName : [user.firstName, Validators.required],
+        lastName : [user.lastName, Validators.required],
+        role: [user.role],
+        id: [user.id]
+      });
+
+      this.loading = false;
+    });
+  }
+
+  onFormSubmit(form) {
+    const user = new User(
+      form.uid,
+      form.firstName,
+      form.lastName,
+      form.role,
+      form.id
+    );
+
+    this.userService.putUser(user).subscribe(() => {
+      this.notificationModalParams = {
+        self: self,
+        type: Notification.SUCCESS,
+        text: 'Profile Information Updated!'
+      };
+
+      this.initializeUserForm();
+    }, (err) => {
+      console.error(err);
+
+      this.initializeUserForm();
+    });
+  }
 }
