6f4d9ac82fb9d04d74f415eb3b0f8d706d733169
Dylan Cornelius <dylan.cornelius@fotlinc.com>
2019-07-30 18:00:18 -0500

    Made current route nav-buttons more visible

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,6 +5,7 @@ import { environment } from '../../environments/environment';
 import { fadeInAnimation } from '../animations';
 import { User } from '../user/user.model';
 import { AuthService } from '../user/auth.service';
+import { Router, RouterEvent } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -17,11 +18,19 @@ export class HeaderComponent implements OnInit {
   title: string;
   user: Observable<User>;
   isLoggedIn: Observable<boolean>;
+  route: string;
 
   constructor(
+    private router: Router,
     private authService: AuthService,
     private userService: UserService,
-  ) {}
+  ) {
+    this.router.events.subscribe((event: RouterEvent) => {
+      if (event.url) {
+        this.route = event.url;
+      }
+    });
+  }
 
   ngOnInit() {
     this.title = environment.config.title;
