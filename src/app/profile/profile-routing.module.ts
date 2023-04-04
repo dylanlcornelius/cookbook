37e51f856e59370ce10d6bebdc8f3a252d4ec1ee
Dylan Cornelius <dylanlcornelius@gmail.com>
2023-04-04 18:39:00 -0500

    Added links to profile settings on profile list for admins

--- src/app/profile/profile-routing.module.ts
+++ src/app/profile/profile-routing.module.ts
@@ -6,6 +6,7 @@ import { ProfileListComponent } from './profile-list/profile-list.component';
 
 const routes: Routes = [
   {path: 'settings', component: ProfileComponent, data: {selectedTabIndex: 0}},
+  {path: 'settings/:id', component: ProfileComponent, data: {selectedTabIndex: 0}},
   {path: 'analytics', component: ProfileComponent, data: {selectedTabIndex: 1}},
   {path: 'recipe-history', component: ProfileComponent, data: {selectedTabIndex: 2}},
   {path: 'list', component: ProfileListComponent}
