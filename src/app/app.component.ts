38622b0979eda738835ab43e33ad4d92afc2c898
Dylan Cornelius <dylanlcornelius@gmail.com>
2023-10-07 11:47:24 -0500

    fix #346 by defaulting to light theme for anonymous users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,6 +23,7 @@ export class AppComponent implements OnInit {
     this.isLoggedIn = this.currentUserService.getIsLoggedIn();
     this.isGuest = this.currentUserService.getIsGuest();
 
+    document.body.classList.add('light');
     this.user.subscribe(user => {
       if (user.uid) {
         if (user.theme) {
