5b63893ea7d5b4aadb6290982b9bd2fb2e94e2ab
Dylan Cornelius <dylanlcornelius@gmail.com>
2024-08-03 12:16:50 -0500

    Update google sign in button

---
+++ src/app/user/google-sign-in/google-sign-in.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-google-sign-in',
+  templateUrl: './google-sign-in.component.html',
+  styleUrls: ['./google-sign-in.component.scss'],
+})
+export class GoogleSignInComponent {}
